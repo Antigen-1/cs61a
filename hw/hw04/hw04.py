945b5203eac808c9cfaa26f7b0fd8191c005c82b
Antigen-1 <zhanghao@antigen.top>
2023-09-02 16:06:24 +0800

    finish hw04

--- hw/hw04/hw04.py
+++ hw/hw04/hw04.py
@@ -51,13 +51,13 @@ def end(s):
 def planet(size):
     """Construct a planet of some size."""
     assert size > 0
-    "*** YOUR CODE HERE ***"
+    return ['planet', size]
 
 
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
-    "*** YOUR CODE HERE ***"
+    return w[1]
 
 
 def is_planet(w):
@@ -117,7 +117,12 @@ def balanced(m):
     >>> check(HW_SOURCE_FILE, 'balanced', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    def sub_balanced(a):
+        s = end(a)
+        return True if is_planet(s) else balanced(s)
+    def torque(a):
+        return total_weight(end(a)) * length(a)
+    return torque(left(m)) == torque(right(m)) and sub_balanced(left(m)) and sub_balanced(right(m))
 
 
 def totals_tree(m):
@@ -149,7 +154,7 @@ def totals_tree(m):
     >>> check(HW_SOURCE_FILE, 'totals_tree', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    return tree(total_weight(m), []) if is_planet(m) else tree(total_weight(m), [totals_tree(end(left(m))), totals_tree(end(right(m)))]) 
 
 
 def replace_loki_at_leaf(t, lokis_replacement):
@@ -181,7 +186,7 @@ def replace_loki_at_leaf(t, lokis_replacement):
     >>> laerad == yggdrasil # Make sure original tree is unmodified
     True
     """
-    "*** YOUR CODE HERE ***"
+    return tree(lokis_replacement if is_leaf(t) and label(t) == 'loki' else label(t), [replace_loki_at_leaf(s, lokis_replacement) for s in branches(t)])
 
 
 def has_path(t, word):
@@ -215,7 +220,7 @@ def has_path(t, word):
     False
     """
     assert len(word) > 0, 'no path for empty word.'
-    "*** YOUR CODE HERE ***"
+    return label(t) == word[0] and (len(word) == 1 or True in [has_path(b, word[1:]) for b in branches(t)])
 
 
 def preorder(t):
@@ -228,7 +233,7 @@ def preorder(t):
     >>> preorder(tree(2, [tree(4, [tree(6)])]))
     [2, 4, 6]
     """
-    "*** YOUR CODE HERE ***"
+    return [label(t)] + sum([preorder(b) for b in branches(t)], [])
 
 
 def str_interval(x):
@@ -252,12 +257,12 @@ def interval(a, b):
 
 def lower_bound(x):
     """Return the lower bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[0]
 
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[1]
 
 
 def str_interval(x):
@@ -276,24 +281,24 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
-    "*** YOUR CODE HERE ***"
+    return interval(lower_bound(x) - upper_bound(y), upper_bound(x) - lower_bound(y))
 
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
-    "*** YOUR CODE HERE ***"
+    assert lower_bound(y) * upper_bound(y) > 0
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -318,8 +323,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(0.5, 1)  # Replace this line!
+    r2 = interval(1, 3)  # Replace this line!
     return r1, r2
 
 
